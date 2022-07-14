7cd817ee04542b830fbad6d621a077c38f9762c1
regmarmcem <tknktknktknktknktknk@gmail.com>
2022-07-14 23:02:01 +0900

    [add] stdoutpipe

--- process/process.go
+++ process/process.go
@@ -1,9 +1,11 @@
-package main 
+package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func main() {
@@ -11,9 +13,22 @@ func main() {
 		return
 	}
 	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	// if cmd is "sleep 1000", execution exit immediately but process keep running
+
+	stdout, _ := cmd.StdoutPipe()
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			fmt.Printf("(stdout) %s\n", scanner.Text())
+		}
+	}()
 	err := cmd.Start()
 	if err != nil {
 		panic(err)
 	}
+	
+	// if cmd is "cat test.txt", it finishes in 3 seconds and text is shown through stdoutpipe
+	time.Sleep(time.Second * 3)
+	
 	fmt.Printf(" Process: %d\n", cmd.Process)
 }
